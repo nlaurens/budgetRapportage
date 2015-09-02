550cdbfec107888bf7677859157250f8a6e35634
Niels Laurens <nlaurens@gmail.com>
2015-09-02 11:01:55 +0200

    adds copy function

--- BoekingsRegel.py
+++ BoekingsRegel.py
@@ -9,7 +9,7 @@ class BoekingsRegel():
         self.documentnummer = 0
         self.personeelsnummer = 0
         self.kosten = 0
-        self.boekjar = 0
+        self.jaar = 0
         self.periode = 0
         self.waarde = 0
         self.tiepe = ''  # Obligo / Geboekt
@@ -20,4 +20,21 @@ class BoekingsRegel():
         print self.kosten
         print self.omschrijving
 
+    def copy(self):
+        new = BoekingsRegel()
+        new.order = self.order
+        new.kostensoort = self.kostensoort
+        new.naamkostensoort = self.naamkostensoort
+        new.omschrijving = self.omschrijving
+        new.datum = self.datum
+        new.documentnummer = self.documentnummer
+        new.personeelsnummer = self.personeelsnummer
+        new.kosten = self.kosten
+        new.jaar = self.jaar
+        new.periode = self.periode
+        new.waarde = self.waarde
+        new.tiepe = self.tiepe
+
+
+        return new
 
