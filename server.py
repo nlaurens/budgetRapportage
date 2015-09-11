8b2715c45db5680afb1de2d37ea52ba5a1c81bc2
Niels Laurens <nlaurens@gmail.com>
2015-09-11 13:50:49 +0200

    removes debug

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ Somday/Maybe:
 - http://bl.ocks.org/NPashaP/96447623ef4d342ee09b
 """
 import web
-#web.config.debug = False #must be done before the rest.
+web.config.debug = False #must be done before the rest.
 import model
 import GrootBoek
 import GrootBoekGroep
