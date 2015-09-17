0786084f9f3fd6dfbe9aa0af197cc9cb8817caf7
Niels Laurens <nlaurens@gmail.com>
2015-09-17 14:54:39 +0200

    adds template for webreport page

---
+++ webreport.py
@@ -0,0 +1,9 @@
+import web
+from config import config
+
+def groep_report():
+    report = {}
+    report['settings'] = 'settings!!'
+    report['summary'] = 'summary!!'
+    report['body'] = 'body!'
+    return report
