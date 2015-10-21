34a2ba5e6a6a0bc0d9753b7fe90225e4356432c2
Niels Laurens <nlaurens@gmail.com>
2015-10-21 12:15:05 +0200

    adds way to group regels in a class that is able to sort them by any attribute in a dictionary for you

--- BoekingsRegel.py
+++ BoekingsRegel.py
@@ -1,3 +1,4 @@
+#TODO Replace BoekingsRegel met 'Regel'
 class BoekingsRegel():
 
 
---
+++ RegelList.py
@@ -0,0 +1,44 @@
+class RegelList():
+
+
+    def __init__(self, regels):
+        self.regels = regels
+
+    '''
+    Returns a dictionary of RegelList sorted out by a specific
+    attributes in the regel.
+    For example: 
+        split_by_regel_attributes(self, ['order', 'kostensoort'])
+    will return:
+        dict['order1']['kostensoort1'] = RegelList
+        dict['order1']['kostensoort..'] = RegelList
+        dict['order2']['kostensoort1'] = RegelList
+        dict['order2']['kostensoort..'] = RegelList
+        dict['order..']['kostensoort..'] = RegelList
+    '''
+    def split_by_regel_attributes(self, attributes_to_group):
+        regelListDict = self.split_by_regel_attribute(attributes_to_group.pop(0))
+            
+        if attributes_to_group:
+            for key, regelListDictChild  in regelListDict.iteritems():
+                regelListDict[key] = regelListDictChild.split_by_regel_attributes(list(attributes_to_group)) #list(..) creates a copy for recursion!
+
+        return regelListDict
+
+
+    def split_by_regel_attribute(self, attribute_to_group):
+        regelDict = {}
+        # Sort out regels for each attribute to group
+        for regel in self.regels:
+            regel_key = getattr(regel, attribute_to_group)
+            if regel_key not in regelDict:
+                regelDict[regel_key] = []
+            regelDict[regel_key].append(regel)
+
+        # Replace the regel list with a RegelList class
+        regelListDict = {}
+        for key, regels in regelDict.iteritems():
+            regelListDict[key] = RegelList(regels)
+
+        return regelListDict
+
