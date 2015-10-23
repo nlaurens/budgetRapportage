4d5dd5e06092e61c8ab72032bd1b6f09b1b930c9
Niels Laurens <nlaurens@gmail.com>
2015-10-23 16:13:34 +0200

    converts websalaris to the new webreport (should be renamed later)

--- websalaris.py
+++ websalaris.py
@@ -3,6 +3,7 @@ TODO
     * Change glyph on collapse:
         http://www.bootply.com/73101
     * Collapse/Expand orders
+    * Figpage
 
 """
 import web
@@ -11,115 +12,6 @@ import GrootBoekGroep
 import GrootBoek
 import model
 import numpy as np
-from RegelList import RegelList
-
-def get_begroting_geboekt(userID, groepstr, jaar):
-    regels = model.get_salaris_geboekt_regels(jaar)
-    regelsGeboekt = RegelList(regels)
-
-    regels = model.get_salaris_begroot_regels(jaar)
-    regelsBegroot = RegelList(regels)
-
-    return regelsGeboekt, regelsBegroot
-
-def correlate_personeelsnummers(regelsBegroot, regelsGeboekt):
-# Cross personeelsnummers begroting -> boekingsnummers
-    begroot = regelsBegroot.split_by_regel_attributes(['personeelsnummer'])
-    kosten = regelsGeboekt.split_by_regel_attributes(['personeelsnummer'])
-
-    matchpersoneelsnummers = {} # personeelsnummer in kosten: { regels begroot}
-    noMatchPerOrder = {} # order : {regelList met regels}
-    for begrootpersoneelsnummer, begrootRegelsList in begroot.iteritems():
-        begrootpersoneelsnummer = list(begrootpersoneelsnummer.strip())
-
-        if begrootpersoneelsnummer:
-            matchfound = False
-            if begrootpersoneelsnummer[0] == '2':
-                begrootpersoneelsnummer[0] = '9'
-            elif begrootpersoneelsnummer[0] == '1':
-                begrootpersoneelsnummer[0] = '8'
-            begrootpersoneelsnummer = ''.join(begrootpersoneelsnummer)
-
-            if long(begrootpersoneelsnummer) in kosten.keys():
-                matchpersoneelsnummers[long(begrootpersoneelsnummer)] = begrootRegelsList
-                matchfound = True
-
-        if not matchfound or not begrootpersoneelsnummer:
-            begrootRegelsDictPerOrder = begrootRegelsList.split_by_regel_attributes(['order'])
-            for order, begrootRegelsList in begrootRegelsDictPerOrder.iteritems():
-                if order not in noMatchPerOrder:
-                    noMatchPerOrder[order] = begrootRegelsList
-                else:
-                    noMatchPerOrder[order].extend(begrootRegelsList)
-
-    return matchpersoneelsnummers, noMatchPerOrder
-
-def cmd_output(userID, groepstr, jaar):
-    regelsGeboekt, regelsBegroot = get_begroting_geboekt(userID, groepstr, jaar)
-    matchpersoneelsnummers, noMatchPerOrder = correlate_personeelsnummers(regelsBegroot, regelsGeboekt)
-
-
-# dict per order per persnr
-    kostenDict = regelsGeboekt.split_by_regel_attributes(['order', 'personeelsnummer'])
-    totalBegroot = 0
-    totalGeboekt = 0
-    for order in kostenDict.keys():
-        begroot = 0
-        totalOrderGeboekt = 0
-        totalOrderBegroot = 0
-        for personeelsnummer, regelsGeboekt in kostenDict[order].iteritems():
-            naamGeboekt = regelsGeboekt.regels[0].personeelsnaam
-            geboekt = regelsGeboekt.total()
-            totalOrderGeboekt +=  geboekt
-
-            naamBegroot = '' # Reset begroot to not found
-            begroot = 0
-
-            if personeelsnummer in matchpersoneelsnummers:
-                persoonbegroot = matchpersoneelsnummers[personeelsnummer].split_by_regel_attributes(['order'])
-                if order in persoonbegroot:
-                    begroot = persoonbegroot[order].total()
-                    naamBegroot = persoonbegroot[order].regels[0].personeelsnaam
-                    totalOrderBegroot +=  begroot
-
-            print '  order %s - persnr %s - kosten %i - begroot %i (%s - %s)' % (order, personeelsnummer, geboekt, begroot, naamGeboekt, naamBegroot)
-        
-        if order in noMatchPerOrder:
-            for regel in noMatchPerOrder[order].regels:
-                print '  order %s - persnr %s - kosten %i - begroot %i (%s - %s)' % (order, regel.personeelsnummer, 0, regel.kosten, '', regel.personeelsnaam)
-                totalOrderBegroot += regel.kosten
-            del noMatchPerOrder[order] #Remove so we end up with a list of remaining begrotingsposten
-
-        print 'Total order %s - geboekt %i begroot %i' % (order, totalOrderGeboekt, totalOrderBegroot)
-        print ''
-        totalBegroot += totalOrderBegroot
-        totalGeboekt += totalOrderGeboekt
-
-    print '--------------'
-    print 'Begroot maar geen kosten gemaakt op order'
-    print ''
-
-    for order, regelList in noMatchPerOrder.iteritems():
-        totalOrderBegroot = 0
-        for regel in regelList.regels:
-            print '  order %s - persnr %s - kosten %i - begroot %i (%s - %s)' % (regel.order, regel.personeelsnummer, 0, regel.kosten, '', regel.personeelsnaam)
-            totalOrderBegroot += regel.kosten
-
-        totalBegroot += totalOrderBegroot
-
-        print 'Total order %s - geboekt %i begroot %i' % (order, totalOrderGeboekt, totalOrderBegroot)
-        print ''
-
-    print 'TOTAL geboekt %i begroot %i' % (totalGeboekt, totalBegroot)
-
-
-
-#-------------------------
-#OLD WEBREPORT:
-#-------------------------
-
-
-
 
 
 def table_string(value):
@@ -167,34 +59,30 @@ def parse_order(order, descr, jaar, render):
     root = GrootBoek.load(order, grootboek, jaar, [])
     root.set_totals()
     html_rows = []
-#TODO DUMMY code
     totals_order = {}
-    totals_order['begroot'] = 0
-    totals_order['realisatie'] = 0
-    totals_order['resultaat'] = 0 
-    for i in range(0,2):
-        begroot = 10000
-        realisatie = 20000
-        resultaat = -10000
-        row = {}
-        row['personeelsnummer'] = 'persnNR'
-        row['naam'] = 'naam persoon'
-        row['begroot'] = begroot
-        row['realisatie'] = realisatie
-        row['resultaat'] = resultaat
-        html_rows.append(personeel_regel_to_html(row, render))
+    totals_order['begroot'] = model.get_plan_totaal(jaar, order)
+    totals_order['realisatie'] = -1*(root.totaalGeboektTree)
+    totals_order['obligo'] = -1*(root.totaalObligosTree)
+    totals_order['resultaat'] = -1*(root.totaalGeboektTree + root.totaalObligosTree) - totals_order['begroot']
 
-        totals_order['begroot'] += begroot
-        totals_order['realisatie'] += realisatie
-        totals_order['resultaat'] += resultaat
+#TODO DUMMY code
+    for i in range(0,2):
+        row = {}
+        row['personeelsnummer'] = 'dummy txt'
+        row['naam'] = 'dummy txt'
+        row['begroot'] = 5000
+        row['realisatie'] = 5000
+        row['resultaat'] = 10000
+        html_rows.append(personeel_regel_to_html(row, render))
 
     header = {}
     header['name'] = descr
+    header['userHash'] = userHash
     header['id'] = order
-#TODO
-    header['img'] = 'NO'
+    header['img'] = ('../static/figs/'+str(jaar)+'-detailed/1-' + str(order) + '.png')
     header['begroot'] = table_string(totals_order['begroot'])
     header['realisatie'] =  table_string(totals_order['realisatie'])
+    header['obligo'] = table_string(totals_order['obligo'])
     header['resultaat'] = table_string(totals_order['resultaat'])
 
     order_table = render.salaris_table_order(html_rows, header)
@@ -237,6 +125,29 @@ def parse_groep(root, jaar, render):
     return order_tables, groepheader, groeprows, groeptotal
 
 
+def fig_html(root, render, jaar):
+    figs = ''
+    if not root.children:
+        graphs = []
+        i = 0
+        for order, descr in root.orders.iteritems():
+            graph = {}
+            graph['link'] = ('../view/' + userHash + '/' + str(order))
+            graph['png'] = ('../static/figs/'+str(jaar)+'-detailed/1-' + str(order) + '.png')
+            #if i%2:
+            #    graph['spacer'] = '</tr><tr>'
+            #else:
+            #    graph['spacer'] = ''
+            graph['spacer'] = '</tr><tr>'
+            graphs.append(graph)
+            i +=1
+
+        figs = render.report_figpage(graphs)
+        return figs
+    else:
+        return None
+
+
 def table_html(root, render, jaar):
     table = []
     childtable = []
@@ -267,12 +178,6 @@ def settings_html(root, render, jaar):
     return render.salaris_settings(lastupdate, buttons, form)
 
 def groep_report(userID, render, groepstr, jaar):
-#NEW websalaris
-    report = {}
-    regelsGeboekt, regelsBegroot = get_begroting_geboekt(userID, groepstr, jaar)
-    matchpersoneelsnummers, noMatchPerOrder = correlate_personeelsnummers(regelsBegroot, regelsGeboekt)
-
-#OLD report
     global userHash 
     userHash = userID
     grootboekgroepfile = 'data/grootboekgroep/LION'
@@ -283,12 +188,12 @@ def groep_report(userID, render, groepstr, jaar):
         root = GrootBoekGroep.load(grootboekgroepfile)
 
     body = table_html(root, render, jaar)
+    figs = fig_html(root, render, jaar)
     settings = settings_html(root, render, jaar)
 
     report = {}
-#ADD collapse/open all button
-#ADD jaar, groep, warning sap ding
     report['settings'] = settings
+    report['figpage'] = figs
     report['summary'] = "<a href='../static/figs/"+str(jaar)+"-detailed/1-" + groepstr + ".png' target='_blank'><img class='img-responsive' src='../static/figs/"+str(jaar)+"-detailed/1-"+groepstr+".png'></a>"
     report['body'] = body
     return report
