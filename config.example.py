8b1d4f00418e5b2a43e8f85d2f1e6ef924b8cd6e
Niels Laurens <nlaurens@gmail.com>
2016-02-01 16:25:32 +0100

    adds example config for new db layout

--- config.example.py
+++ config.example.py
@@ -1,12 +1,73 @@
 config = {
-    "IpRanges": '127.0.0.1 127.255.255.255 10.0.0.1 10.255.255.255'
-    "globalPW": '<global password string'>,
-    "lastSAPexport": '<last date of the SAP export>',
-    "salt": '<random string>',
+    "IpRanges": '127.0.0.0 127.255.255.255 10.0.0.1 10.255.255.255',
+    "globalPW": '<PASSWORD>',
+    "lastSAPexport": '15 Sep 2015',
+    "salt": '<UNIQUE SALT>',
     "mysql": {
-        "user": "<user name>",
-        "pass": "<pw>",
-        "db": "<name of the db>"
-        "host": "<addres of the mysql host>"
+        "user": "<USER NAME>",
+        "pass": "<PASSWORD>",
+        "db": "<DB NAME>",
+        "host": ""
+    },
+    "SAPkeys": {
+        "types":{
+            "order": "int",
+            "kostensoort": "int",
+            "kostensoortnaam": "varchar(255)",
+            "kosten": "decimal(19,4)",
+            "jaar": "int",
+            "periode": "int",
+            "omschrijving": "varchar(255)",
+            "documentnummer": "int",
+            "invoerdatum": "varchar(255)",
+        },
+        "geboekt":{
+            "order": "Order",
+            "kostensoort": "Kostensoort",
+            "kostensoortnaam": "Naam v. kostensoort",
+            "kosten": "Waarde/CO-valuta",
+            "jaar": "Boekjaar",
+            "periode": "Periode",
+            "omschrijving": "Omschrijving",
+            "documentnummer": "Documentnummer",
+            "invoerdatum": "Invoerdatum",
+        },
+        "obligo":{
+            "order": "Order",
+            "kostensoort": "Kostensoort",
+            "kostensoortnaam": "Naam v. kostensoort",
+            "kosten": "Waarde/CO-valuta",
+            "jaar": "Boekjaar",
+            "periode": "Periode",
+            "omschrijving": "Omschrijving",
+            "documentnummer": "Nr. referentiedoc.",
+        },
+        "plan":{
+            "order": "Order",
+            "kostensoort": "Kostensoort",
+            "kostensoortnaam": "Naam v. kostensoort",
+            "jaar": "Boekjaar",
+            "documentnummer": "Documentnummer",
+            "kosten": "Totaalwrd./vslg.val.",
+        },
+        "salaris":{
+            "order": "Kostenpl.",
+            "ordernaam": "Omschr. Kost.pl.",
+            "kostensoort": "Gr.boek",
+            "kostensoortnaam": "omschr.g.boekrek.",
+            "kosten": "Bedrag",
+            "personeelsnummer": "PersNr",
+            "personeelsnaam": "Naam",
+            "schaal": "Schaal",
+            "trede": "Tr.",
+        },
+        "salaris_begroting":{
+            "order": "Order",
+            "kosten": "Kosten",
+            "personeelsnummer": "Personeelsnummer",
+            "personeelsnaam": "Naam",
+            "schaal": "Schaal",
+            "trede": "Trede",
+        },
     }
 }
