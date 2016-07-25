b9d18be6615ca6f186a3c3565fe052b3c5d9d549
Niels Laurens <nlaurens@gmail.com>
2016-07-25 12:12:40 +0200

    updates example config

--- mysql-import.bat
+++
@@ -1,8 +0,0 @@
-echo off
-setlocal enabledelayedexpansion
-FOR %%f IN ("*.csv") DO (
-  set old=%%~dpnxf
-  set new=!old:\=\\!
-  mysql -e "load data local infile '"!new!"' IGNORE into table <db>.<table> COLUMNS TERMINATED BY ','" -u root
-  echo %%~nxf DONE
-)
