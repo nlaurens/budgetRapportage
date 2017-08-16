02b60e558310eb630bc9deb92c3189bc74f3dbf3
Niels Laurens <nlaurens@gmail.com>
2017-08-16 14:43:21 +0200

    adds todos for security checks on serving files

--- controller/graph.py
+++ controller/graph.py
@@ -13,20 +13,33 @@ class Graph:
         self.order_allowed = True  # TODO security
         self.path = None
 
+
     def GET(self, year, graph_type, name):
-        if graph_type == 'realisatie' or graph_type == 'overview':
+        self.year = year
+        self.graph_type = graph_type
+        self.name = name
+
+        types_allowed = ['realisatie', 'overview']
+        years_allowed = [2017]  # TODO add security by only adding years in db
+        names_allowed = [ 'xx', 'yy']  #TODO add security by adding all groups/orders to this list
+
+        if self.graph_type not in types_allowed:
             self.path = os.path.join(config['graphs']['path'], year, graph_type, name+'.png')
 
-        if os.path.isfile(self.path):
-            return self.serve_image()
-        else:
-            generated = True
-            # Try to generate the image
-            if generated:
+            if os.path.isfile(self.path):
                 return self.serve_image()
+            else:
+                if self.create_image():
+                    return self.serve_image()
 
         raise web.notfound()
+
 
     def serve_image(self):
         web.header("Content-Type", "images/png")
         return open(self.path, "rb").read()
+
+
+    def create_image(self):
+
+        return False
